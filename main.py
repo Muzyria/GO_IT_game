54787b381283ae9fe56ea739de8e7c86f7974dbe
Muzyria <fila090580@gmail.com>
2023-03-02 22:38:11 +0200

    new

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import random
+import time
 
 import pygame
 from pygame.constants import QUIT
 
 pygame.init()
-
 screen = width, height = 800, 600
-
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
-
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
@@ -19,22 +17,20 @@ ball_speed = [1, 1]
 
 is_working = True
 while is_working:
+    # time.sleep(0.01)
     for event in pygame.event.get():
         if event.type == QUIT:
             is_working = False
 
     ball_rect = ball_rect.move(ball_speed)
-
     if ball_rect.bottom >= height or ball_rect.top <= 0:
         ball_speed[1] = -ball_speed[1]
         ball.fill((random.randrange(256), random.randrange(256), random.randrange(256)))
-
-    if ball_rect.left >= height or ball_rect.right <= 0:
+    if ball_rect.left >= width or ball_rect.right <= 0:
         ball_speed[0] = -ball_speed[0]
         ball.fill((random.randrange(256), random.randrange(256), random.randrange(256)))
 
     main_surface.fill(BLACK)
     main_surface.blit(ball, ball_rect)
-
     # main_surface.fill((155, 155, 155))
     pygame.display.flip()
