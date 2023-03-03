c448315234f438f77be140a9b5e6dc69c6b3cac9
Muzyria <fila090580@gmail.com>
2023-03-03 22:19:36 +0200

    new

--- main_3.py
+++ main_3.py
@@ -13,6 +13,7 @@ screen = width, height = 800, 600
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
+GREEN = 0, 255, 0
 
 main_surface = pygame.display.set_mode(screen)
 
@@ -30,9 +31,20 @@ def create_enemy():
     return [enemy, enemy_rect, enemy_speed]
 
 
+def create_bonus():
+    bonus = pygame.Surface((20, 20))
+    bonus.fill(GREEN)
+    bonus_rect = pygame.Rect(random.randint(0, width), 0, *bonus.get_size())
+    bonus_speed = random.randint(2, 5)
+    return [bonus, bonus_rect, bonus_speed]
+
+
 CREATE_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_ENEMY, 1500)
+CREATE_BONUS = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_BONUS, 1500)
 
+bonuses = []
 enemies = []
 
 is_working = True
@@ -47,32 +59,38 @@ while is_working:
         if event.type == CREATE_ENEMY:
             enemies.append(create_enemy())
 
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
+
     pressed_keys = pygame.key.get_pressed()
-
     main_surface.fill(BLACK)
-
     main_surface.blit(ball, ball_rect)
+
     for enemy in enemies:
         enemy[1] = enemy[1].move(-enemy[2], 0)
         main_surface.blit(enemy[0], enemy[1])
-
         if enemy[1].left < 0:
-            # enemies.pop(enemies.index(enemy))
+            enemies.remove(enemy)
+        if ball_rect.colliderect(enemy[1]):
             enemies.remove(enemy)
 
-    if pressed_keys[K_DOWN]:
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(0, bonus[2])
+        main_surface.blit(bonus[0], bonus[1])
+        if bonus[1].bottom > height:
+            bonuses.remove(bonus)
+        if ball_rect.colliderect(bonus[1]):
+            bonuses.remove(bonus)
+        print(len(bonuses))
+
+    if pressed_keys[K_DOWN] and not ball_rect.bottom >= height:
         ball_rect = ball_rect.move((0, ball_speed))
-
-    if pressed_keys[K_UP]:
+    if pressed_keys[K_UP] and not ball_rect.top <= 0:
         ball_rect = ball_rect.move((0, -ball_speed))
-
-    if pressed_keys[K_RIGHT]:
+    if pressed_keys[K_RIGHT] and not ball_rect.right >= width:
         ball_rect = ball_rect.move((ball_speed, 0))
-
-    if pressed_keys[K_LEFT]:
+    if pressed_keys[K_LEFT] and not ball_rect.left <= 0:
         ball_rect = ball_rect.move((-ball_speed, 0))
-
-
 
     # main_surface.fill((155, 155, 155))
     pygame.display.flip()
