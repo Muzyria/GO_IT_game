6bb7bf0629ecc962b63c017b066dfdc6e6af3e4f
Muzyria <fila090580@gmail.com>
2023-03-05 00:08:48 +0200

    new

--- main_3.py
+++ main_3.py
@@ -28,7 +28,7 @@ player_rect = player.get_rect()
 player_speed = 5
 
 enemy_img = pygame.image.load("img/enemy.png").convert_alpha()
-bonus_img = pygame.image.load("img/bonus.png").convert_alpha()
+bonus_img = pygame.image.load("img/bonus_2.png").convert_alpha()
 
 
 def create_enemy():
